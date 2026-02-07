bbabda853981af25fcfe909566cea5d4937bc674
Faidat Egberinde <faidategberinde@gmail.com>
2026-02-07 13:24:10 +0100

    Added the cart overlay

--- mainWebsitePage/mainWebsitePage.js
+++ mainWebsitePage/mainWebsitePage.js
@@ -5,6 +5,10 @@ function clickme() {
 const navbar = document.getElementById("navBar");
 const searchInput = document.getElementById("searchInput");
 const closeSearch = document.getElementById("closeSearch");
+const cartIcon = document.getElementById("cart");
+const closeCart = document.getElementById("closeCart");
+const cartWrapper = document.querySelector(".cartWrapper");
+const cartOverlay = document.getElementById("cartOverlay");
 
 searchInput.addEventListener("focus", () => {
   navbar.classList.add("search-active");
@@ -14,4 +18,14 @@ searchInput.addEventListener("focus", () => {
 closeSearch.addEventListener("click", () => {
   navbar.classList.remove("search-active");
   searchInput.blur();
+});
+
+cartWrapper.addEventListener("click", () => {
+    cartOverlay.classList.add("active");
+    document.body.classList.add("cart-open");
+});
+
+closeCart.addEventListener("click", () => {
+    cartOverlay.classList.remove("active");
+    document.body.classList.remove("cart-open");
 });
